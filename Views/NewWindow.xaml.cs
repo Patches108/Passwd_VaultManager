98d88052ea4258e773f15940a2d2e75dfb504b6f
Patches108 <stillalivemayans@gmail.com>
2025-09-25 19:21:07 +0100

    New Vault Window Implementations

--- Views/NewWindow.xaml.cs
+++ Views/NewWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Passwd_VaultManager.ViewModels;
+using System.Diagnostics;
 using System.Windows;
 
 namespace Passwd_VaultManager.Views {
@@ -8,6 +9,7 @@ namespace Passwd_VaultManager.Views {
     public partial class NewWindow : Window {
 
         private readonly NewWindowVM _vm = new();
+        private int _bitRate = 128;
 
         public NewWindow() {
             InitializeComponent();
@@ -26,6 +28,52 @@ namespace Passwd_VaultManager.Views {
             var gen = new Passwd_VaultManager.Funcs.PasswdGen();
             string pw = gen.GenPassword(bitRate: 128, len: 16); // returns at least 21 chars here
             txtPasswd.Text = pw;
+        }
+
+        private void cmdCreateVault_Click(object sender, RoutedEventArgs e) {
+            // 1. Checks for and disallows empty controls.
+            // 2. Creates vault.
+            // 3. Saves it to DB.
+        }
+
+        private void rad_128_Click(object sender, RoutedEventArgs e) {
+            _bitRate = 128;
+            Debug.WriteLine($"_bitRate: {_bitRate}");
+        }
+
+        private void rad_256_Click(object sender, RoutedEventArgs e) {
+            _bitRate = 256;
+            Debug.WriteLine($"_bitRate: {_bitRate}");
+        }
+
+        private void txtAppName_GotFocus(object sender, RoutedEventArgs e) {
+            if(String.IsNullOrEmpty(txtAppName.Text) || txtAppName.Text.Equals("Website/Application Name"))
+                txtAppName.Text = String.Empty;
+        }
+
+        private void txtUserName_GotFocus(object sender, RoutedEventArgs e) {
+            if (String.IsNullOrEmpty(txtUserName.Text) || txtUserName.Text.Equals("User Name"))
+                txtUserName.Text = String.Empty;
+        }
+
+        private void txtPasswd_GotFocus(object sender, RoutedEventArgs e) {
+            if (String.IsNullOrEmpty(txtPasswd.Text) || txtPasswd.Text.Equals("Passwd"))
+                txtPasswd.Text = String.Empty;
+        }
+
+        private void txtAppName_LostFocus(object sender, RoutedEventArgs e) {
+            if (String.IsNullOrEmpty(txtAppName.Text))
+                txtAppName.Text = "Website/Application Name";
+        }
+
+        private void txtUserName_LostFocus(object sender, RoutedEventArgs e) {
+            if (String.IsNullOrEmpty(txtUserName.Text))
+                txtUserName.Text = "User Name";
+        }
+
+        private void txtPasswd_LostFocus(object sender, RoutedEventArgs e) {
+            if (String.IsNullOrEmpty(txtPasswd.Text))
+                txtPasswd.Text = "Passwd";
         }
     }
 }
