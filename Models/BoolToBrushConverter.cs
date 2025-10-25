06f56ccd02a5640b8ff440d4d5bff7e444c353ad
Patches108 <stillalivemayans@gmail.com>
2025-10-25 10:01:40 +0100

    Styling updates to Password panel. User-friendly inhancements.

---
+++ Models/BoolToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Passwd_VaultManager.Models {
+    public class BoolToBrushConverter : IValueConverter {
+        public Brush TrueBrush { get; set; } = Brushes.LimeGreen;
+        public Brush FalseBrush { get; set; } = Brushes.IndianRed;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is bool b)
+                return b ? TrueBrush : FalseBrush;
+
+            return FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotSupportedException();
+        }
+    }
+}
